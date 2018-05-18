dfb623f1e6ab408507052c5d9a3e85079cddadea
Blanke <blankeeee@gmail.com>
2018-05-18 16:22:52 +0800

    init commit ,add AlarmManager code.

---
+++ app/src/main/java/com/blanke/scheduleproject/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.blanke.scheduleproject
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("main", "onCreated")
+        bu_alarm?.setOnClickListener {
+            alarm()
+        }
+    }
+
+    private fun alarm() {
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        Log.d("main", "time=${System.currentTimeMillis()}")
+        // 一次性闹钟
+        alarmManager.set(AlarmManager.RTC, System.currentTimeMillis() + 10000, getTestActivityIntent())
+        Toast.makeText(this, "10s 后会重启 MainActivity", Toast.LENGTH_SHORT).show()
+//        finish()
+
+        // >19 精确
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 10000, getTestReceiverIntent())
+
+        // > 23 低耗电模式精确
+        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 10000, getTestReceiverIntent())
+
+        // 重复闹钟
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 3000, 5000, getTestReceiverIntent())
+        // 跟省电，会合并闹钟时间，间隔时间可能没那么精准
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 3000, 5000, getTestReceiverIntent())
+    }
+
+    private fun getTestActivityIntent(): PendingIntent {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        return PendingIntent.getActivity(applicationContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+
+    private fun getTestReceiverIntent(): PendingIntent {
+        val intent = Intent(applicationContext, TestReceiver::class.java)
+        return PendingIntent.getBroadcast(applicationContext, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+}
